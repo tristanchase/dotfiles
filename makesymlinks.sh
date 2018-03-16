f41ce5c7558b52516806acc4a0903055eca6e01c
Tristan Chase <tristan.m.chase@gmail.com>
2018-03-16 03:57:27 -0400

    Change mv to create numbered backups.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -26,7 +26,8 @@ echo "done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 echo "Moving any existing dotfiles from ~ to $olddir ..."
 for file in $files; do
-    mv -i ~/.$file ~/dotfiles_old/
+    #mv -i ~/.$file ~/dotfiles_old/
+    mv --backup=t ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
